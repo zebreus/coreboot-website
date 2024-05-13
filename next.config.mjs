8e2cb301d4b62480ffc036c023026254be53f142
Zebreus <lennart@zebre.us>
2024-05-13 22:42:48 +0200

    Try to fix github pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: { unoptimized: true },
   output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
